7d86448703fd1a7e65563d6784e4f990c8c61971
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2025-09-24 17:17:19 +0530

    Check

--- virtualoffice/apps/http-server/src/routes/v1/index.ts
+++ virtualoffice/apps/http-server/src/routes/v1/index.ts
@@ -1,34 +1,104 @@
 import { Router } from "express";
 import { userRouter } from "./user";
-import { adminRouter } from "./admin";
 import { spaceRouter } from "./space";
+import { adminRouter } from "./admin";
+import { SigninSchema, SignupSchema } from "../../types";
+import {hash, compare} from "../../scrypt";
+import client from "@repo/db/client";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "../../config";
+
 export const router = Router();
 
-router.post("/signup", (req, res) => {
-    res.json({
-        message: "Signup"
-    });
-});
+router.post("/signup", async (req, res) => {
+    console.log("inside signup")
+    // check the user
+    const parsedData = SignupSchema.safeParse(req.body)
+    if (!parsedData.success) {
+        console.log("parsed data incorrect")
+        res.status(400).json({message: "Validation failed"})
+        return
+    }
 
-router.post("/login", (req, res) => {
-    res.json({
-        message: "Login"
-    });
-});
+    const hashedPassword = await hash(parsedData.data.password)
 
-router.get("/avatar", (req, res) => {
-    res.json({
-        message: "Logout"
-    });
-});
+    try {
+         const user = await client.user.create({
+            data: {
+                username: parsedData.data.username,
+                password: hashedPassword,
+                role: parsedData.data.type === "admin" ? "Admin" : "User",
+            }
+        })
+        res.json({
+            userId: user.id
+        })
+    } catch(e) {
+        console.log("erroer thrown")
+        console.log(e)
+        res.status(400).json({message: "User already exists"})
+    }
+})
 
-router.get("//elements", (req, res) => {                                                                                      
-    res.json({
-        message: "Logout"
-    });
-});
+router.post("/signin", async (req, res) => {
+    const parsedData = SigninSchema.safeParse(req.body)
+    if (!parsedData.success) {
+        res.status(403).json({message: "Validation failed"})
+        return
+    }
 
+    try {
+        const user = await client.user.findUnique({
+            where: {
+                username: parsedData.data.username
+            }
+        })
+        
+        if (!user) {
+            res.status(403).json({message: "User not found"})
+            return
+        }
+        const isValid = await compare(parsedData.data.password, user.password)
 
-router.use("/user",userRouter);
-router.use("/admin",adminRouter);
-router.use("/space",spaceRouter);   
+        if (!isValid) {
+            res.status(403).json({message: "Invalid password"})
+            return
+        }
+
+        const token = jwt.sign({
+            userId: user.id,
+            role: user.role
+        }, JWT_PASSWORD);
+
+        res.json({
+            token
+        })
+    } catch(e) {
+        res.status(400).json({message: "Internal server error"})
+    }
+})
+
+router.get("/elements", async (req, res) => {
+    const elements = await client.element.findMany()
+
+    res.json({elements: elements.map(e => ({
+        id: e.id,
+        imageUrl: e.imageUrl,
+        width: e.width,
+        height: e.height,
+        static: e.static
+    }))})
+})
+
+router.get("/avatars", async (req, res) => {
+    const avatars = await client.avatar.findMany()
+    res.json({avatars: avatars.map(x => ({
+        id: x.id,
+        imageUrl: x.imageUrl,
+        name: x.name
+    }))})
+})
+
+router.use("/user", userRouter)
+router.use("/space", spaceRouter)
+router.use("/admin", adminRouter)
